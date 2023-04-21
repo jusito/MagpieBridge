e37882e0dfe4a6c514f0a9f9672c1fa4a4931876
Lucas Briese <lucas.briese@iem.fraunhofer.de>
2023-04-21 14:06:55 +0200

    test: loosened restrictive npm dependency

--- src/test/java/magpiebridge/projectservice/npm/NpmProjectServiceTest.java
+++ src/test/java/magpiebridge/projectservice/npm/NpmProjectServiceTest.java
@@ -12,6 +12,8 @@ import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -22,8 +24,7 @@ import org.junit.Test;
  */
 public class NpmProjectServiceTest {
 
-  private static final Path rootPath =
-      Paths.get("src", "test", "resources", "DemoProjectNpm").toAbsolutePath();
+  private static final Path rootPath = Paths.get("src", "test", "resources", "DemoProjectNpm").toAbsolutePath();
 
   private static String npmCommand = "npm";
 
@@ -35,12 +36,11 @@ public class NpmProjectServiceTest {
       if (System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH).contains("win")) {
         npmCommand = "npm.cmd";
       }
-      int exitCode =
-          new ProcessBuilder(npmCommand, "--version")
-              .directory(rootPath.toFile())
-              .inheritIO()
-              .start()
-              .waitFor();
+      int exitCode = new ProcessBuilder(npmCommand, "--version")
+          .directory(rootPath.toFile())
+          .inheritIO()
+          .start()
+          .waitFor();
       npmExists = exitCode == 0;
     } catch (IOException e) {
       npmExists = false;
@@ -48,20 +48,25 @@ public class NpmProjectServiceTest {
     assumeTrue("Npm is not installed", npmExists);
 
     // install the project to download packages to system
-    int exitCode =
-        new ProcessBuilder(npmCommand, "install")
-            .directory(rootPath.toFile())
-            .inheritIO()
-            .start()
-            .waitFor();
+    int exitCode = new ProcessBuilder(npmCommand, "install")
+        .directory(rootPath.toFile())
+        .inheritIO()
+        .start()
+        .waitFor();
     assertEquals(0, exitCode);
   }
 
-  @Test
-  public void test() {
+  private NpmProjectService npmProjectService;
+
+  @Before
+  public void before() {
     Logger.getLogger(NpmProjectService.class.getName()).setLevel(Level.FINE);
-    NpmProjectService npmProjectService = new NpmProjectService();
+    npmProjectService = new NpmProjectService();
     npmProjectService.setRootPath(rootPath);
+  }
+
+  @Test
+  public void testProjectServiceInitialized() {
     assertEquals("npm", npmProjectService.getProjectType());
     assertTrue(npmProjectService.getDependencyPath().isPresent());
     assertTrue(npmProjectService.getPackageJson().isPresent());
@@ -69,11 +74,15 @@ public class NpmProjectServiceTest {
     assertEquals(rootPath.resolve("node_modules"), npmProjectService.getDependencyPath().get());
     assertEquals(1, npmProjectService.getDirectDependencies().get().size());
     assertEquals(1, npmProjectService.getDependencies().size());
-    assertTrue(npmProjectService.getDependency("lodash").isPresent());
+  }
+
+  @Test
+  public void testInstalledNpmPackageLodash() {
     NpmPackage npmPackage = npmProjectService.getDependency("lodash").get();
     assertFalse(npmPackage.getDependencies().isPresent());
     assertTrue(npmPackage.getPath().isPresent());
     assertEquals(rootPath.resolve("node_modules").resolve("lodash"), npmPackage.getPath().get());
-    assertEquals("4.17.19", npmPackage.getVersion());
+    System.out.println(npmPackage.getVersion());
+    assertTrue(npmPackage.getVersion().matches("^4\\.17\\..*$"));
   }
 }
